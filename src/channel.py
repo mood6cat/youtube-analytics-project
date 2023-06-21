e1c02ef7e152244f09e14795cd815c3eb0115a94
dant9 <dant982@mail.ru>
2023-06-22 01:44:34 +0500

    MaGiCa!

--- src/channel.py
+++ src/channel.py
@@ -1,10 +1,18 @@
+import json
+import os
 
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
+import isodate
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
+
         pass
 
     def print_info(self) -> None:
