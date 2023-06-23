1a02a1d5ef845cf2cfbc9b2c6b287637e006acb4
dant9 <dant982@mail.ru>
2023-06-23 17:45:17 +0500

    Не могу вывести какую-то инфу

--- src/channel.py
+++ src/channel.py
@@ -7,8 +7,8 @@ from googleapiclient.discovery import build
 import isodate
 
 
-api_key: str = os.getenv('YT_API_KEY')
-
+# api_key: str = os.getenv('YT_API_KEY')  Это не работало
+api_key: str = 'AIzaSyAT2fssSmUegKbhed6Yt9hkBfV0IKVnELo'   # то что сгенерировал API гугла. А это работает
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -18,19 +18,23 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id  #id канала
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        self.content = None
-        self.info = None
-        self.title = None
-        self.description = None
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']  # количество подписчиков
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']  # количество видео
+        self.viewCount = self.channel['items'][0]['statistics']['viewCount']  # общее количество просмотров
+        self.title: str = self.channel['items'][0]['snippet']['title']   # Название канала
+        self.description: str = self.channel['items'][0]['snippet']['description']  # Описание канала
         self.url = "https://www.youtube.com/channel/" + channel_id  # ссылка на канал
 
-        pass
+
+    def printj(dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
     def my_channel_id(self):
+        # print(self.channel_id)
         return self.channel_id
 
     def print_info(self):
-        """Выводит в консоль информацию о канале.
-         just touch set_info and print self.info"""
-        self.info = json.dumps(self.content, indent=2, ensure_ascii=False)
-        return self.info
+        """Выводит в консоль информацию о канале."""
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel
