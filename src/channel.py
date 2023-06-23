8311f1c4229a2244c3eeda78964e28c45b8d8fb2
dant9 <dant982@mail.ru>
2023-06-23 15:17:25 +0500

    Пошли ошибки!

--- src/channel.py
+++ src/channel.py
@@ -6,15 +6,31 @@ from googleapiclient.discovery import build
 
 import isodate
 
+
+api_key: str = os.getenv('YT_API_KEY')
+
+
 class Channel:
     """Класс для ютуб-канала"""
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.channel_id = channel_id  #id канала
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.content = None
+        self.info = None
+        self.title = None
+        self.description = None
+        self.url = "https://www.youtube.com/channel/" + channel_id  # ссылка на канал
 
         pass
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+    def my_channel_id(self):
+        return self.channel_id
+
+    def print_info(self):
+        """Выводит в консоль информацию о канале.
+         just touch set_info and print self.info"""
+        self.info = json.dumps(self.content, indent=2, ensure_ascii=False)
+        return self.info
