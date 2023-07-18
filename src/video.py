6b0320db3599c8fcaf53aa8477e19dad86383b6a
dant9 <dant982@mail.ru>
2023-07-18 18:36:49 +0500

    It's work

--- src/video.py
+++ src/video.py
@@ -1,46 +1,30 @@
-import json
+import os
 
 import src
 
 from src.channel import Channel
 
+from googleapiclient.discovery import build
 
-class Video():
-
-    def __init__(self, video_id):
-        """
-        Инициализация атрибутов класса + дополнительный
-        защищенный атрибут video_data в котором все данные
-        **Если введено неверное ID видео, обрабатываем ошибку
-        IndexError, все поля кроме video_id выставляем в None
-        """
+import googleapiclient.discovery
+api_key: str = 'AIzaSyAT2fssSmUegKbhed6Yt9hkBfV0IKVnELo'  # то что сгенерировал API гугла. А это работает
+class Video:
+    def __init__(self, video_id: str) -> None:
         self.video_id = video_id
-        try:
-            self.__video_data = src.channel.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                                  id=video_id).execute()
-            self.title = self.__video_data["items"][0]["snippet"]["title"]
-            self.video_link = f'https://www.youtube.com/watch?v={video_id}'
-            self.view_count = self.__video_data["items"][0]["statistics"]["viewCount"]
-            self.like_count = self.__video_data["items"][0]["statistics"]["likeCount"]
+        self.api_key: str = 'AIzaSyAT2fssSmUegKbhed6Yt9hkBfV0IKVnELo'
+        # создать специальный объект для работы с API
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.video_response = self.youtube.videos().list(part='snippet,statistics', id=video_id).execute()
+        self.title = self.video_response['items'][0]['snippet']['title']
+        self.url = 'https://www.youtube.com/channel/' + video_id
+        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
-        except IndexError:
-            self.__video_data = None
-            self.title = None
-            self.video_link = None
-            self.view_count = None
-            self.like_count = None
-
+    def __str__(self):
+        return f'{self.title}'
 
 # Объявляем дочерний от Video класс PLVideo
 class PLVideo(Video):
-    def __init__(self, video_id, playlist_id):
-        """
-        Инициализация атрибутов класса + дополнительный
-        защищенный атрибут playlist_data в котором все данные
-        """
+    def __init__(self, video_id, playlist_id) -> None:
         super().__init__(video_id)
         self.playlist_id = playlist_id
-        self.__playlist_data = src.channel.youtube.playlistItems().list(playlistId=playlist_id,
-                                                                        part='contentDetails,snippet',
-                                                                        maxResults=50,
-                                                                        ).execute()
